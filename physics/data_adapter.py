be47838130766bad23774ae5e6871b4ae2ecc3cd
caitken5 <caitken5@student.ubc.ca>
2026-02-04 09:12:47 -0800

    Updating data_adapter.py to remove errors

--- physics/data_adapter.py
+++ physics/data_adapter.py
@@ -2,15 +2,6 @@
 
 from physics.constants import FEET_TO_METERS, METERS_TO_FEET, INCHES_TO_METERS, METERS_TO_INCHES
 
-
-def parse_array_json(s):
-    # Replace bare nan with null (JSON compatible)
-    s = s.replace('nan', 'null')
-    return np.array(json.loads(s), dtype=np.float32)
-
-for col in keypoint_names:
-    df[col] = df[col].apply(parse_array_json)
-
 def ft_to_m(x):
     return x * FEET_TO_METERS
 
